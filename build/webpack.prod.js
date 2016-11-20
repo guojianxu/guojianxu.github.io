c9a6704cc545214ba0c757829d0d72b0552fd0b1
徐健国 <xujianguo02@meituan.com>
2016-11-20 16:43:32 +0800

    webpack添加压缩插件

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,5 +1,25 @@
 var webpack = require('webpack');
 var config = require('./webpack.config');
+
+config.plugins = config.plugins || [];
+
+config.plugins.push(
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      keep_fargs: true,
+      sequences: true,
+      dead_code: true,
+      conditionals: true,
+      booleans: true,
+      unused: true,
+      if_return: true,
+      join_vars: true,
+      drop_console: true
+    }
+  })
+);
+
 var compiler = webpack(config);
 
 function callback(err, stats) {
